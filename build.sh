f44c921d21eff7251cda61e93d9e69ac7d2020c9
Z4xk3r07h <30985576+TheProdigyLeague@users.noreply.github.com>
2025-05-11 21:36:33 -0400

    ERR: no dockerfile

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-
-docker build -t xenserver/xenserver-build-env .
+[+] Building 0.1s (1/1) FINISHED                                                                                                                                                                                              docker:default
+ => [internal] load build definition from Dockerfile                                                                                                                                                                                    0.0s
+ => => transferring dockerfile: 2B                                                                                                                                                                                                      0.0s
+ERROR: failed to solve: failed to read dockerfile: open Dockerfile: no such file or directory
